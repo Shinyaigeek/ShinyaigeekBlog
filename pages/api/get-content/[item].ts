d4c31a679a09dec3532a24bceac8aaff6674ca62
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-03 18:57:22 +0900

    [add]headings

--- pages/api/get-content/[item].ts
+++ pages/api/get-content/[item].ts
@@ -11,7 +11,16 @@ export default (req: Http2ServerRequest, res: Http2ServerResponse) => {
   );
   const fmedItem = fm(rawItem);
   const header = fmedItem.attributes;
-  const body = marked(fmedItem.body).replace(/\n/g, "<br>");
+  const content = marked(fmedItem.body).replace(/\n/g, "<br>");
+  const headings: string[] | null = content.match(/<h2 id=".+?">.+?<\/h2>/g);
+  console.log(headings, "iiiii");
+  let links: string = "";
+  headings &&
+    headings.forEach(link => {
+      const name = link.split('"')[1];
+      links += `<a href="#${name}">${name}</a>`;
+    });
+  const body = links + content;
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.statusCode = 200;
