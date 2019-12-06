1abebe83e0662478dd0d493580898d975680723d
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-06 11:36:25 +0900

    [add]getQUeryParam script

--- pages/api/get-items/index.ts
+++ pages/api/get-items/index.ts
@@ -1,26 +1,22 @@
 import fs from "fs";
 import { Http2ServerRequest, Http2ServerResponse } from "http2";
 import fm from "front-matter";
-
-export function getQueryParam(target: string) {
-  let queryParams: any = {};
-  target.split("&").forEach(param => {
-    queryParams[param.split("=")[0]] = param.split("=")[1];
-  });
-  return queryParams;
-}
+import { getQueryParam } from "../../../script/getQueryParam";
 
 export default (req: Http2ServerRequest, res: Http2ServerResponse) => {
   const { url } = req;
   const itemNum = fs.readdirSync("./items").length;
 
-  const queryParams = getQueryParam(url.slice(url.indexOf("?") + 1,url.length));
-  console.log(queryParams)
-  console.log((10 * queryParams.page - 10))
+  const queryParams = getQueryParam(
+    url.slice(url.indexOf("?") + 1, url.length)
+  );
   const itemInfos = Array(itemNum)
     .fill(1)
     .map((item, index, tar) => {
-      const rawItem = fs.readFileSync("./items/" + (index + 1) + ".md", "utf8");
+      const rawItem = fs.readFileSync(
+        "./items/" + (itemNum - index) + ".md",
+        "utf8"
+      );
       return fm(rawItem).attributes;
     });
   res.setHeader("Content-Type", "application/json");
@@ -28,7 +24,11 @@ export default (req: Http2ServerRequest, res: Http2ServerResponse) => {
   res.statusCode = 200;
   res.end(
     JSON.stringify({
-      headers: itemInfos.slice(itemInfos.length - (10 * queryParams.page), itemInfos.length - (10 * queryParams.page - 10))
+      headers: itemInfos.slice(
+        10 * queryParams.page - 10,
+        10 * queryParams.page
+      ),
+      totalItem: itemNum
     })
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,29 +7,52 @@ import Link from "next/link";
 import { header } from "./post/[item]";
 import fetch from "isomorphic-unfetch";
 
+import { getQueryParam } from "../script/getQueryParam"
+
+import { Result, Button, Pagination } from "antd";
+
 import ItemList from "../components/ItemList";
 
 interface Props {
   headers: header[];
+  totalItem:number
 }
 
 const Index: NextPage<Props> = props => {
   return (
     <div>
-      {props.headers.map(header => {
-        return <ItemList {...header} />;
-      })}
+      {props.headers.length !== 0 &&
+        props.headers.map(header => {
+          return <ItemList {...header} />;
+        })}
+      {props.headers.length === 0 && (
+        <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={
+            <Link href="/">
+              <Button type="primary">Back Home</Button>
+            </Link>
+          }
+        />
+      )}
+      <Pagination simple defaultCurrent={2} total={props.totalItem} />
     </div>
   );
 };
 
 Index.getInitialProps = async req => {
-  const res = await fetch("http://localhost:3000/api/get-items/index?page=2", {
-    method: "GET",
-    mode: "cors",
-    credentials: "same-origin",
-    referrer: "no-referrer"
-  });
+  const page = req.query.page || 1;
+  const res = await fetch(
+    "http://localhost:3000/api/get-items/index?page=" + page,
+    {
+      method: "GET",
+      mode: "cors",
+      credentials: "same-origin",
+      referrer: "no-referrer"
+    }
+  );
   const headers: Props = await res.json();
   return headers;
 };
