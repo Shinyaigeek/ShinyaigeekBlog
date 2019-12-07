9d083f2e52bc9d9ced83dbc774ec8b3433d16f2a
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-07 13:51:37 +0900

    [add]date icon

--- components/MetaInfo.tsx
+++ components/MetaInfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Tag } from "antd";
+import { Tag,Icon } from "antd";
 
 export default function MetaInfo(props: {
   tag: string[];
@@ -10,7 +10,7 @@ export default function MetaInfo(props: {
   return (
     <div>
       <h1>{props.name}</h1>
-      <div>{props.date}</div>
+      <div><Icon type="edit" />{props.date}</div>
       {
           props.tag.map((tag) => {
               <Tag>{tag}</Tag>
