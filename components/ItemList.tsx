f06eb9e65ea8f3f7ee0c72d25ff4fe25ce2f84e6
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-11 14:35:38 +0900

    [fix]ItemList

--- components/ItemList.tsx
+++ components/ItemList.tsx
@@ -16,7 +16,7 @@ export default function Item(props: header) {
         className="item--card"
         bordered={true}
         hoverable={true}
-        // cover={<img src={props.img} className="item--card__img" alt="card--icon"/>}
+        cover={<img src={props.img} className="item--card__img" alt="card--icon"/>}
         style={{
           width: 300,
           margin: "24px auto"
