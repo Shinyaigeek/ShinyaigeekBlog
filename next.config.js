9cf0a68b8164657b9e5eb47830428d3ed1617507
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-11 13:49:03 +0900

    [add]set static export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,52 @@
 const withSass = require("@zeit/next-sass");
-const Mode = require('frontmatter-markdown-loader/mode')
+const Mode = require("frontmatter-markdown-loader/mode");
+
+const returnHome = function() {
+  let items = {};
+  const page = "/";
+  items["/"] = {
+    page: page
+  };
+  return items;
+};
+
+const returnPosts = function(itemN) {
+  let posts = {};
+  for (i = itemN; i >= 1; i--) {
+    let path = "/post/" + i;
+    posts[path] = {
+      page: "/post/[item]",
+      query: {
+        item: i
+      }
+    };
+  }
+  return posts;
+};
 
 const nextConfig = {
-  compress:true,
+  compress: true,
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    // const itemN = dirN("./items").length;
+    // const itemList = getFileContent(itemN);
+    const posts = returnPosts(26);
+    const homes = returnHome();
+    const others = {
+      "/privacypolicy": {
+        page: "/privacypolicy"
+      },
+      "/profile": {
+        page: "/profile"
+      }
+    };
+    let all = {};
+    all = Object.assign(others, posts);
+    all = Object.assign(all, homes);
+    return all;
+  },
   webpack: function(config, { dev, defaultLoaders }) {
     config.module.rules.push({
       test: /\.(eot|woff|woff2|ttf|svg|png|jp g|gif|webp)$/,
@@ -17,7 +61,7 @@ const nextConfig = {
       options: {
         mode: [Mode.BODY]
       }
-    })
+    });
 
     return config;
   }
