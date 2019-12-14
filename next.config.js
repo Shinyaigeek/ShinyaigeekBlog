a70437e4a6c2cca6bf26f74e66199f7d3223d253
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-15 01:26:29 +0900

    [add]func to export as static file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,61 @@
 const withSass = require("@zeit/next-sass");
-const withCss = require("@zeit/next-css")
-const Mode = require('frontmatter-markdown-loader/mode')
-const FontminPlugin = require('fontmin-webpack')
-const optimizedImages = require('next-optimized-images');
+const withCss = require("@zeit/next-css");
+const Mode = require("frontmatter-markdown-loader/mode");
+const FontminPlugin = require("fontmin-webpack");
+const optimizedImages = require("next-optimized-images");
+const fs = require("fs");
+
+const dirN = function (dir) {
+    return fs.readdirSync(dir)
+}
+
+const returnHome = function() {
+  let items = {};
+  const page = "/";
+  items["/"] = {
+    page: page
+  };
+  return items;
+};
+
+const returnPosts = function(itemN) {
+  let posts = {};
+  for (i = itemN; i >= 1; i--) {
+    let path = "/post/" + i;
+    posts[path] = {
+      page: "/post/[item]",
+    };
+  }
+  return posts;
+};
 
 const nextConfig = {
-  compress:true,
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    const itemN = dirN("./items").length;
+    const posts = returnPosts(itemN);
+    const homes = returnHome();
+    const others = {
+      "/privacypolicy": {
+        page: "/privacypolicy"
+      },
+      "/profile": {
+        page: "/profile"
+      }
+    };
+    let all = {};
+    all = Object.assign(others, posts);
+    all = Object.assign(all, homes);
+    return all;
+  },
+  compress: true,
   webpack: function(config, { dev, defaultLoaders }) {
     config.module.rules.push({
       test: /\.(eot|woff|woff2|ttf|svg|png|jp g|gif|webp)$/,
       use: {
-        loader: "url-loader",
+        loader: "url-loader"
       }
     });
 
@@ -20,13 +65,13 @@ const nextConfig = {
       options: {
         mode: [Mode.BODY]
       }
-    })
+    });
 
     config.plugins.push(
       new FontminPlugin({
-        autodetect: true,
+        autodetect: true
       })
-    )
+    );
 
     return config;
   }
