586a298f9e77b40cd84449873003d67b4cebe4b2
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-11 00:38:34 +0900

    [fix]status

--- pages/api/ls.ts
+++ pages/api/ls.ts
@@ -3,7 +3,7 @@ import fs from "fs"
 export default async (req:any, res:any) => {
     const ls = await fs.promises.readdir(".")
 
-    res(status).json({
+    res(200).json({
         ls: ls
     })
 }
