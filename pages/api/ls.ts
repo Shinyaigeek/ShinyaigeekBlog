66b05284340bf725e087e3b74e7f64057375494d
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-11 00:59:44 +0900

    {fix]

--- pages/api/ls.ts
+++ pages/api/ls.ts
@@ -4,10 +4,12 @@ export default async (req:any, res:any) => {
     const ls = await fs.promises.readdir("/")
     const ls1 = await fs.promises.readdir("./")
     const ls2 = await fs.promises.readdir("../")
+    const ls3 = await fs.promises.readdir("./.next")
 
     res.status(200).json({
         ls: ls,
         ls1: ls1,
-        ls2:ls2
+        ls2:ls2,
+        ls3: ls3
     })
 }
